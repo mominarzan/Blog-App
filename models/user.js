e8f415de7fa9ba013cc3128e106b3f8622fe99a6
mominarzan <67833509+mominarzan@users.noreply.github.com>
2022-09-22 14:01:02 +0530

    second commit

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const UserSchema = new mongoose.Schema(
 	{
 		email: { type: String, required: true, unique: true },
-		password: { type: String, required: true }
+		passwordh: { type: String, required: true }
 	},
 	{ collection: 'users' }
 )
--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const Article = require('./models/article')
 const Users = require('./models/user')
 const Admin = require('./models/admin')
 const bodyParser = require("body-parser")
-
-//const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt')
 const articleRouter = require('./routes/articles')
 
 
@@ -13,6 +12,8 @@ const methodOverride = require('method-override')
 const { json } = require('express')
 const article = require('./models/article')
 const app = express()
+const saltRounds = 10;
+const salt = bcrypt.genSaltSync(saltRounds);
 
 mongoose.connect('mongodb://localhost:27017/blog1', {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
@@ -26,10 +27,11 @@ app.use(express.json());
 
 app.get('/', async (req, res) => {
   res.render('home')
+  
 })
 
 app.get('/login', async (req, res) => {
-  res.render('user/login')
+ res.render('user/login')
 });
 
 app.post('/login', async (req, res) => {
@@ -37,12 +39,14 @@ app.post('/login', async (req, res) => {
     const email = req.body.email;
     const userpassword = req.body.password;
     const useremail = await Users.findOne({ email: email });
-    if (useremail.password === userpassword) {
-      const articles = await Article.find().sort({ createdAt: 'desc' })
-      res.status(201).render('landpage', { articles: articles });
+    
+    const ismatch = await bcrypt.compare(userpassword,useremail.passwordh); 
+    if (!ismatch) {
+      res.send("password are wrong !!!!");
     }
     else {
-      res.send("password are wrong !!!!");
+      const articles = await Article.find().sort({ createdAt: 'desc' })
+      res.status(201).render('landpage', { articles: articles });
     }
 
   } catch (error) {
@@ -59,17 +63,18 @@ app.get('/register', async (req, res) => {
 app.post('/register', async (req, res) => {
 
   const { email, password, password2 } = req.body;
-
-  //const passwordenc= await bcrypt.hash(password,10);
+  
+  const passwordh = bcrypt.hashSync(password, salt);
+ 
   try {
     let response = await Users.create({
-      email, password
+      email,passwordh
     })
     console.log("sucesssss", response);
     res.render('user/login')
   } catch (error) {
-    if (error.code === 11000) {
-      return res, json({ status: "error", error: "Email Already in existing in database " })
+   if (error.code === 11000) {
+     return res, json({ status: "error", error: "Email Already in existing in database " })
     }
     throw error
   }
@@ -84,8 +89,11 @@ app.post('/adminlogin', async (req, res) => {
   try {
     const email = req.body.email;
     const userpassword = req.body.password;
+
     const useremail = await Admin.findOne({ email: email });
-    if (useremail.password === userpassword) {
+
+    const ismatch = await bcrypt.compare(userpassword,useremail.passwordh);
+    if (ismatch) {
       const articles = await Article.find().sort({ createdAt: 'desc' })
       res.render('articles/index', { articles: articles })
     }
@@ -99,13 +107,6 @@ app.post('/adminlogin', async (req, res) => {
 });
 
 
-
-
-
-
-
-
-
 app.get('/adminregister', async (req, res) => {
   res.render('admin/adminregister')
 })
@@ -114,12 +115,12 @@ app.get('/adminregister', async (req, res) => {
 app.post('/adminregister', async (req, res) => {
 
   const { email, password, password2, key } = req.body;
+  const passwordh = bcrypt.hashSync(password, salt);
 
-  //const passwordenc= await bcrypt.hash(password,10);
   try {
     if (key === "root") {
       let response = await Admin.create({
-        email, password
+        email, passwordh
       })
       console.log("sucesssss", response);
       res.render('admin/adminlogin')
@@ -135,40 +136,10 @@ app.post('/adminregister', async (req, res) => {
   }
 });
 
-
-
-
-
-
-
-
-
-//  app.get('/landpage', async (req, res) => {
-// const articles = await Article.find().sort({ createdAt: 'desc' })
-// res.render('landpage', { articles: articles })
-//  })
-
-
- app.get('/index', async (req, res) => {
+app.get('/index', async (req, res) => {
   const articles = await Article.find().sort({ createdAt: 'desc' })
   res.render('articles/index', { articles: articles })
-  })
-
- // app.post('/like', async (req, res) => {
-  //  let {name,id} = req.params;
-  //  console.log(name);
-  //  console.log(id);
-    
-  //  const blogid = "6328867dbd80b10af4871b72";
- //   const adduserlike = await Article.updateOne( { _id: blogid },
- //     { $inc: { likes: 1 } } )
- //   const userlike = await Article.findOne({_id:blogid});
- //   console.log(userlike.likes);
- //   const articles = await Article.find().sort({ createdAt: 'desc' })
- //   res.render('landpage', { articles: articles })
-  //  });
-
-
+});
 
 
 app.use('/articles', articleRouter)
